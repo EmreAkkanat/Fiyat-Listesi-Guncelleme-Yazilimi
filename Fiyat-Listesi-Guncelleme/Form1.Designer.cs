20173eb076f9b5957a0fbead50cbbba72959e57d
Emre-Akkanat <91632213+EmreAkkanat@users.noreply.github.com>
2023-08-01 13:33:05 +0300

    Fiyat-Listesi-Güncelleme-Yazılımı

---
+++ Fiyat-Listesi-Guncelleme/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace empmakros2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(14, 14);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 25);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Ekle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(1011, 14);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 25);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(895, 18);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(56, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "EURO";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(767, 18);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(62, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "DOLAR";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(641, 18);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(38, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "TL";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox2.Controls.Add(this.dataGridView2);
+            this.groupBox2.Controls.Add(this.dataGridView3);
+            this.groupBox2.Location = new System.Drawing.Point(14, 515);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1206, 380);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Analiz";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(14, 49);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 62;
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(567, 317);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(624, 49);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 62;
+            this.dataGridView3.RowTemplate.Height = 28;
+            this.dataGridView3.Size = new System.Drawing.Size(567, 317);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 44);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1177, 420);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Location = new System.Drawing.Point(36, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1235, 913);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(488, 504);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(264, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Analız";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.radioButton2);
+            this.groupBox3.Controls.Add(this.radioButton3);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Controls.Add(this.radioButton1);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(14, 11);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1205, 498);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1311, 951);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ Fiyat-Listesi-Guncelleme/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System.IO;
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace empmakros2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            anaData(dataGridView1);
+            olmayanData(dataGridView2);
+            yinelenenData(dataGridView3);
+        }
+
+        // VERİ TABANI BAĞLANTISINI YAPTIKTAN SONRA ÇALIŞIR
+
+        SqlConnection baglanti = new SqlConnection("veri tabanı bağlantısı");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView3.AllowUserToAddRows = false;
+            dataGridView2.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToAddRows = false;
+            //---------------------------------------------------Veri Tabanında olmayan ürünler ayırma
+            List<int> list1 = new List<int>();
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                SqlCommand cmd = new SqlCommand("select stok_kodu from tablo_ismi", baglanti);
+                baglanti.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                string dss = "";
+                string deger = "";
+                while (dr.Read())
+                {
+                    deger = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                    if (deger.Equals(dr["stok_kodu"].ToString()))
+                    {
+                        dss = "var";
+                        break;
+                    }
+                    else
+                    {
+                        dss = "yok";
+                    }
+                }
+                if (dss == "yok")
+                {
+                    list1.Add(i);
+                    dataGridView2.Rows.Add(deger);
+                }
+                baglanti.Close();
+            }
+            list1.Reverse();
+            foreach (int item in list1)
+            {
+                dataGridView1.Rows.RemoveAt(item);
+            }
+            list1.Clear();
+            //-------------------------------------------------------------------------------------son
+            //---------------------------------------------------------------Yinelenen ürünleri ayırma
+            List<string> list2 = new List<string>();
+            List<string> list6 = new List<string>();
+            for (int j = 0; j < dataGridView1.RowCount; j++)
+            {
+                string ass = dataGridView1.Rows[j].Cells[0].Value.ToString();
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    list2.Add(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                }
+                list2.RemoveAt(j);
+                for (int k = 0; k < list2.Count; k++)
+                {
+                    string asss = list2.ElementAt(k).ToString();
+                    if (ass == asss)
+                    {
+                        dataGridView3.Rows.Add(ass);
+                        list6.Add(ass);
+                        break;
+                    }
+                }
+                list2.Clear();
+            }
+            for (int h = 0; h < dataGridView1.RowCount; h++)
+            {
+                string hgf = dataGridView1.Rows[h].Cells[0].Value.ToString();
+                for (int p = 0; p < list6.Count; p++)
+                {
+                    string fgh = list6.ElementAt(p);
+                    if (hgf==fgh)
+                    {
+                        dataGridView1.Rows.RemoveAt(h);
+                        h--;
+                        break;
+                    }
+                }
+            }
+            list6.Clear();
+            List<string> list25 = new List<string>();
+            for (int j = 0; j < dataGridView3.RowCount; j++)
+            {
+                for (int i = 0; i < dataGridView3.RowCount; i++)
+                {
+                    list25.Add(dataGridView3.Rows[i].Cells[0].Value.ToString());
+                }
+                string ass = dataGridView3.Rows[j].Cells[0].Value.ToString();
+                list25.RemoveAt(j);
+                for (int k = 0; k < list25.Count; k++)
+                {
+                    string asss = list25.ElementAt(k).ToString();
+                    if (ass == asss)
+                    {
+                        dataGridView3.Rows.RemoveAt(j);
+                        j--;
+                        break;
+                    }
+                }
+                list25.Clear();
+            }
+            //-------------------------------------------------------------------------------------son
+            dataGridView3.Refresh();
+            dataGridView2.Refresh();
+            dataGridView1.Refresh();
+        }
+
+        private void yinelenenData(DataGridView dataGridView3)
+        {
+            dataGridView3.ReadOnly = true;
+            dataGridView3.AllowUserToDeleteRows = true;
+            dataGridView3.RowHeadersVisible = false;
+            dataGridView3.AllowUserToAddRows = false;
+
+            dataGridView3.ColumnCount = 1;
+            dataGridView3.Columns[0].Name = "STOK_KODU";
+
+            dataGridView3.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        private void olmayanData(DataGridView dataGridView2)
+        {
+            dataGridView2.ReadOnly = true;
+            dataGridView2.AllowUserToDeleteRows = true;
+            dataGridView2.RowHeadersVisible = false;
+            dataGridView2.AllowUserToAddRows = false;
+
+            dataGridView2.ColumnCount = 1;
+            dataGridView2.Columns[0].Name = "STOK_KODU";
+
+            dataGridView2.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        private void anaData(DataGridView dataGridView1)
+        {
+            dataGridView1.ReadOnly = false; // sadece okunabilir olması yani veri düzenleme kapalı
+            dataGridView1.AllowUserToDeleteRows = false; // satırların silinmesi engelleniyor
+            dataGridView1.RowHeadersVisible = false; //Gizlenmesini sağlar
+            dataGridView1.AllowUserToAddRows = false;
+
+            dataGridView1.ColumnCount = 4; //Kaç kolon olacağı belirleniyor…
+            dataGridView1.Columns[0].Name = "STOK_KODU";//Kolonların adı belirleniyor
+            dataGridView1.Columns[1].Name = "TL";
+            dataGridView1.Columns[2].Name = "DOLAR";
+            dataGridView1.Columns[3].Name = "EURO";
+
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+
+            if (dataGridView1.RowCount < 1)
+            {
+                dataGridView1.Rows.Add(1);
+            }
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                string s = Clipboard.GetText();
+                string[] lines = s.Replace("\n", "").Split('\r');
+
+                List<string> list = new List<string>(lines);
+                list.RemoveAt(lines.Length - 1);
+                lines = list.ToArray();
+                list.Clear();
+
+            int row = dataGridView1.CurrentCell.RowIndex;
+                int col = dataGridView1.CurrentCell.ColumnIndex;
+                if (lines.Length > 1)
+                {
+                    dataGridView1.Rows.Add(lines.Length-1);
+                }
+
+                string[] fields;
+                foreach (string item in lines)
+                {
+                    //dataGridView1.Rows.Add(1);
+                    fields = item.Split('\t');
+                    foreach (string f in fields)
+                    {
+                        dataGridView1[col, row].Value = f;
+                        col++;
+                    }
+                    row++;
+                    col = dataGridView1.CurrentCell.ColumnIndex;
+                }
+            }
+            else if (e.Control && e.KeyCode == Keys.N)
+            {
+                dataGridView1.Rows.Add(1);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            if (dataGridView1.RowCount < 1)
+            {
+                dataGridView1.Rows.Add(1);
+            }
+            dataGridView2.Rows.Clear();
+            dataGridView3.Rows.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlDataAdapter da = new SqlDataAdapter();
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                String insertData = "INSERT INTO tablo_ismi(stok_kodu, tl, dolar, euro) VALUES (@stok_kodu, @tl, @dolar, @euro)";
+                SqlCommand cmd = new SqlCommand(insertData, baglanti);
+                cmd.Parameters.AddWithValue("@stok_kodu", dataGridView1.Rows[i].Cells[0].Value);
+                cmd.Parameters.AddWithValue("@tl", dataGridView1.Rows[i].Cells[1].Value);
+                cmd.Parameters.AddWithValue("@dolar", dataGridView1.Rows[i].Cells[2].Value);
+                cmd.Parameters.AddWithValue("@euro", dataGridView1.Rows[i].Cells[3].Value);
+                da.InsertCommand = cmd;
+                cmd.ExecuteNonQuery();
+            }
+            baglanti.Close();
+            MessageBox.Show("İşleminiz başarıyla gerçekleştirilmiştir");
+        }
+    }
+}
